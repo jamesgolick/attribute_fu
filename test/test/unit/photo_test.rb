83771a34aa20385ad5c2eb5f4dcf076357d5edb6
james <james@80b79608-713f-0410-8737-d8c0d0c1b50c>
2007-11-21 19:19:40 +0000

    test for basic behaviour of has_many :attributes => true association
    git-svn-id: http://svn.jamesgolick.com/attribute_fu/trunk@13 80b79608-713f-0410-8737-d8c0d0c1b50c

--- test/test/unit/photo_test.rb
+++ test/test/unit/photo_test.rb
@@ -2,4 +2,40 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
   should_have_many :comments
+  
+  context "comment_attributes" do
+    setup do
+      @photo = Photo.create
+      @gob = @photo.comments.create :author => "Gob Bluth",  :body => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed..."
+      @bob = @photo.comments.create :author => "Bob Loblaw", :body => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed..."
+      
+      @photo.comment_attributes = { @gob.id.to_s => { :author => "Buster Bluth", :body => "I said it was _our_ nausia..." },
+                                    :new         => { "0" => { :author => "George-Michael", :body => "I was going to smoke the marijuana like a ciggarette." }},   
+                                    :remove      => [@bob.id.to_s]}
+      @photo.save
+    end
+    
+    context "with existing child" do
+      setup do
+        @gob.reload
+      end
+
+      should "update attributes" do
+        assert_equal "Buster Bluth", @gob.author, "Author attribute of child model was not updated."
+        assert_equal "I said it was _our_ nausia...", @gob.body, "Body attribute of child model was not updated."
+      end
+    end
+    
+    context "with new hash" do
+      should "create new comment" do
+        assert @photo.comments.any? { |comment| comment.author == "George-Michael" && comment.body =~ "was going to smoke" }, "New comment was not created."
+      end
+    end
+    
+    context "with remove hash" do
+      should "remove those children from the parent" do
+        assert @photo.comments.any? { |comment| comment.author == "Bob Loblaw" }, "Comment in remove array was not removed."
+      end
+    end
+  end
 end
