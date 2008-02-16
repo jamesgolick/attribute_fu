e68a4f617f12068e47cda76497a9484004cd2db9
james <james@80b79608-713f-0410-8737-d8c0d0c1b50c>
2008-02-16 19:38:39 +0000

    remove unnecessary test stuff
    git-svn-id: http://svn.jamesgolick.com/attribute_fu/trunk@57 80b79608-713f-0410-8737-d8c0d0c1b50c

--- test/test/test_helper.rb
+++ test/test/test_helper.rb
@@ -3,8 +3,4 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 require 'mocha'
 
-class Test::Unit::TestCase
-  self.use_transactional_fixtures = true
-  self.use_instantiated_fixtures  = false
-  fixtures :all
-end
+class Test::Unit::TestCase; end
